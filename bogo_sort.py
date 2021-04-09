f2e1fcda62eb4b2aa58f82e6bb5b8d711049a2c6
bonn1112 <akutsu0521@gmail.com>
2021-04-09 11:41:05 -0600

    bogo_sort

---
+++ bogo_sort.py
@@ -0,0 +1,19 @@
+import random
+from typing import List
+
+
+def in_order(numbers: List[int]) -> bool:
+    for i in range(len(numbers) - 1):
+        if numbers[i] > numbers[i + 1]:
+            return False
+    return True
+
+
+def bogo_sort(numbers: List[int]) -> List[int]:
+    while not in_order(numbers):
+        random.shuffle(numbers)
+    return numbers
+
+
+if __name__ == '__main__':
+    print(bogo_sort([1, 5, 3, 4, 6]))
