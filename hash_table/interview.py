c84ff42e57aab2ff7d56b2c79e971bad4902e694
bonn1112 <akutsu0521@gmail.com>
2021-04-20 22:09:17 -0600

    try 1

--- hash_table/interview.py
+++ hash_table/interview.py
@@ -6,32 +6,17 @@ from typing import List, Tuple, Optional
 def get_pair(numbers: List[int], target: int) -> Optional[Tuple[int, int]]:
     cache = set()
     for num in numbers:
-        cache.add(num)
-        val = target - num
-        if val in cache:
-            return val, num
+        var = target - num
+        if var in cache:  # varがcacheにある場合
+            return var, num
+        cache.add(num)  # 12 - 11 = 1, 12 - 2 = 10, .... 12 - 10 = 2, which in cache
 
 
-def get_pair_half_sum(numbers: List[int]) -> Optional[Tuple[int, int]]:
-    sum_numbers = sum(numbers)
-    half_sum, remainder = divmod(sum_numbers, 2) ###
-    if remainder != 0:
-        return
-
-    # if sum_numbers % 2 != 0:
-    #     return
-    # half_sum = int(sum_numbers / 2)
-
-    cache = set()
-    for num in numbers:
-        cache.add(num)
-        val = half_sum - num
-        if val in cache:
-            return val, num
+# def get_pair_half_sum(numbers: List[int]) -> Optional[Tuple[int, int]]:
 
 
 if __name__ == '__main__':
     l = [11, 2, 5, 9, 10, 3]
     t = 12
     print(get_pair(l, t))
-    print(get_pair_half_sum(l))
+    # print(get_pair_half_sum(l))
